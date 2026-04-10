e809c828383351e52af6769b63d399d63768e447
bhumidoshi28 <bhumi.doshi1@somaiya.edu>
2026-04-10 06:13:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Azure"
+    return "Hello Azure App Running!"
 
-app.run()
+app.run(host="0.0.0.0", port=8000)
